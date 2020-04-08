ecb718b1a3bf58049a4f3db1325d925ba3a3e0a4
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2020-04-08 17:56:18 +0200

    Added location model.

---
+++ Life-The-Game/Models/location.py
@@ -0,0 +1,12 @@
+class Location:
+    def __init__(self):
+        self.__x = 0
+        self.__y = 0
+
+    @property
+    def X(self):
+        return self.__x
+
+    @property
+    def Y(self):
+        return self.__y
