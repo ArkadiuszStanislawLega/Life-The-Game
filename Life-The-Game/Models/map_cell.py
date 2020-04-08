7b0c0a8eeba0cc707b10d8a6d722731b0cd208ab
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2020-04-08 18:11:35 +0200

    Add map cell model.

---
+++ Life-The-Game/Models/map_cell.py
@@ -0,0 +1,16 @@
+from location import Location
+from life_cell import LifeCell
+
+
+class MapCell:
+    def __init__(self):
+        self.__is_occupied = False
+        self.__location = Location()
+
+    @property
+    def is_occupied(self):
+        return self.__is_occupied
+
+    @property
+    def location(self):
+        return self.__location
